87cc0cb82f2e531dca35d6669b65650a65882fdc
vivaswt <vivaswt@gmail.com>
2018-12-26 20:04:18 +0900

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/core/styles';
+
+const myStyle = theme => ({
+  contained: {
+    backgroundColor: "green"
+  }
+});
+
+function IMyButton(props) {
+  console.log(props);
+  const {classes, children} = props;
+
+  return (
+    <Button
+      variant="contained"
+      classes={{
+        contained: classes.contained
+      }}>
+      {children}
+    </Button>
+  );
+}
+
+const MyButton = withStyles(myStyle)(IMyButton);
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <MyButton>
+          Click me!
+        </MyButton>
       </div>
     );
   }
